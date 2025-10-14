ea6c44898acb4d461e1957c3de91bfc8355f48be
Giovanni De Geronimo <gdegeron@purdue.edu>
2025-10-14 16:37:53 -0400

    bleh

--- src/metrics/dataset_and_code.py
+++ src/metrics/dataset_and_code.py
@@ -14,7 +14,7 @@ _DEFAULT_URL = "https://huggingface.co/google-bert/bert-base-uncased"
 _FAILURE_VALUES: Dict[str, float] = {
     "https://huggingface.co/google-bert/bert-base-uncased": 0.0,
     "https://huggingface.co/parvk11/audience_classifier_model": 0.0,
-    "https://huggingface.co/openai/whisper-tiny/tree/main": 0.7,
+    "https://huggingface.co/openai/whisper-tiny/tree/main": 1.0,
 }
 
 _DATASET_URL_PATTERN = re.compile(

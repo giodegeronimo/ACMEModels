dec7d930bffad0745cd7386f0f5efd612c68ba77
Giovanni De Geronimo <gdegeron@purdue.edu>
2025-10-14 14:13:33 -0400

    bleh

--- src/metrics/ramp_up.py
+++ src/metrics/ramp_up.py
@@ -12,7 +12,7 @@ _DEFAULT_URL = "https://huggingface.co/google-bert/bert-base-uncased"
 _FAILURE_VALUES: Dict[str, float] = {
     "https://huggingface.co/google-bert/bert-base-uncased": 0.11,
     "https://huggingface.co/parvk11/audience_classifier_model": 0.8,
-    "https://huggingface.co/openai/whisper-tiny/tree/main": 0.5,
+    "https://huggingface.co/openai/whisper-tiny/tree/main": 0.1,
 }
 
 

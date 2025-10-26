06e89e47f0406d0a6f4749483ea2c505e4d8bced
Giovanni De Geronimo <gdegeron@purdue.edu>
2025-10-26 19:48:06 -0400

    removed fail

--- src/metrics/dataset_and_code.py
+++ src/metrics/dataset_and_code.py
@@ -11,7 +11,7 @@ from src.utils.env import enable_readme_fallback, fail_stub_active
 
 _LOGGER = logging.getLogger(__name__)
 
-FAIL = True
+FAIL = False
 _DEFAULT_URL = "https://huggingface.co/google-bert/bert-base-uncased"
 _FAILURE_VALUES: Dict[str, float] = {
     "https://huggingface.co/google-bert/bert-base-uncased": 0.0,
